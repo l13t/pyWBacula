1725bb8e0572e1c256b0fa81c88db0fb0dd5e544
Liet Kynes <liet@yggdrasil.lo0.org.ua>
2015-11-04 01:41:04 +0200

    Add semantic ui

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
+from flask.ext.bower import Bower
+
+Bower(app)
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -2,5 +2,10 @@ from flask import render_template
 from app import app
 
 @app.route('/')
+@app.route('/home')
 def index():
   return render_template("index.html", title='Home page')
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
