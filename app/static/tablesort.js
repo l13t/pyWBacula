0838a489c32de84317c8e146570e68240cca6a1c
Liet Kynes <liet@yggdrasil.lo0.org.ua>
2016-02-11 17:20:12 +0200

    add backup duration report per pool

--- app/static/tablesort.js
+++ app/static/tablesort.js
@@ -1,209 +1,253 @@
-/*
-  A simple, lightweight jQuery plugin for creating sortable tables.
-  https://github.com/kylefox/jquery-tablesort
-  Version 0.0.1
-*/
+;(function() {
+  function Tablesort(el, options) {
+    if (!(this instanceof Tablesort)) return new Tablesort(el, options);
 
-;(function($) {
+    if (!el || el.tagName !== 'TABLE') {
+      throw new Error('Element must be a table');
+    }
+    this.init(el, options || {});
+  }
 
-  $.tablesort = function ($table, settings) {
-    var self = this;
-    this.$table = $table;
-    this.settings = $.extend({}, $.tablesort.defaults, settings);
-    this.$table.find('thead th').bind('click.tablesort', function() {
-      if( !$(this).hasClass('disabled') ) {
-        self.sort($(this));
-      }
-    });
-    this.index = null;
-    this.$th = null;
-    this.direction = [];
-  };
+  var sortOptions = [];
 
-  $.tablesort.prototype = {
+  var createEvent = function(name) {
+    var evt;
 
-    sort: function(th, direction) {
-      var start = new Date(),
-        self        = this,
-        table       = this.$table,
-        rows        = table.find('tbody tr'),
-        index       = th.index(),
-        cache       = [],
-        fragment    = $('<div/>'),
-        sortValueForCell = function(th, td, sorter) {
-          var
-            sortBy
-          ;
-          if(th.data().sortBy) {
-            sortBy = th.data().sortBy;
-            return (typeof sortBy === 'function')
-              ? sortBy(th, td, sorter)
-              : sortBy
-            ;
-          }
-          return ( td.data('sort') )
-            ? td.data('sort')
-            : td.text()
-          ;
-        },
-        naturalSort =  function naturalSort (a, b) {
-          var
-            chunkRegExp    = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
-            stripRegExp    = /(^[ ]*|[ ]*$)/g,
-            dateRegExp     = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
-            numericRegExp  = /^0x[0-9a-f]+$/i,
-            oRegExp        = /^0/,
-            cLoc           = 0,
-            useInsensitive = function(string) {
-              return ('' + string).toLowerCase().replace(',', '');
-            },
-            // convert all to strings strip whitespace
-            x              = useInsensitive(a).replace(stripRegExp, '') || '',
-            y              = useInsensitive(b).replace(stripRegExp, '') || '',
-            // chunk/tokenize
-            xChunked       = x.replace(chunkRegExp, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
-            yChunked       = y.replace(chunkRegExp, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
-            chunkLength    = Math.max(xChunked.length, yChunked.length),
-            // numeric, hex or date detection
-            xDate          = parseInt(x.match(numericRegExp), 10) || (xChunked.length != 1 && x.match(dateRegExp) && Date.parse(x)),
-            yDate          = parseInt(y.match(numericRegExp), 10) || xDate && y.match(dateRegExp) && Date.parse(y) || null,
-            xHexValue,
-            yHexValue,
-            index
-          ;
-          // first try and sort Hex codes or Dates
-          if (yDate) {
-            if( xDate < yDate ) {
-              return -1;
-            }
-            else if ( xDate > yDate ) {
-              return 1;
-            }
-          }
-          // natural sorting through split numeric strings and default strings
-          for(index = 0; index < chunkLength; index++) {
-              // find floats not starting with '0', string or 0 if not defined (Clint Priest)
-              xHexValue = !(xChunked[index] || '').match(oRegExp) && parseFloat(xChunked[index]) || xChunked[index] || 0;
-              yHexValue = !(yChunked[index] || '').match(oRegExp) && parseFloat(yChunked[index]) || yChunked[index] || 0;
-              // handle numeric vs string comparison - number < string - (Kyle Adams)
-              if (isNaN(xHexValue) !== isNaN(yHexValue)) {
-                return ( isNaN(xHexValue) )
-                  ? 1
-                  : -1
-                ;
-              }
-              // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
-              else if (typeof xHexValue !== typeof yHexValue) {
-                xHexValue += '';
-                yHexValue += '';
-              }
-              if (xHexValue < yHexValue) {
-                return -1;
-              }
-              if (xHexValue > yHexValue) {
-                return 1;
-              }
-          }
-          return 0;
-        }
-      ;
-
-      if(rows.length === 0) {
-        return;
-      }
-
-      self.$table.find('thead th').removeClass(self.settings.asc + ' ' + self.settings.desc);
-
-      this.$th = th;
-      if(this.index != index) {
-        this.direction[index] = 'desc';
-      }
-      else if(direction !== 'asc' && direction !== 'desc') {
-        this.direction[index] = this.direction[index] === 'desc' ? 'asc' : 'desc';
-      }
-      else {
-        this.direction[index] = direction;
-      }
-      this.index = index;
-      direction = this.direction[index] == 'asc' ? 1 : -1;
-
-      self.$table.trigger('tablesort:start', [self]);
-      self.log("Sorting by " + this.index + ' ' + this.direction[index]);
-
-      rows.sort(function(a, b) {
-        var aRow = $(a);
-        var bRow = $(b);
-        var aIndex = aRow.index();
-        var bIndex = bRow.index();
-
-        // Sort value A
-        if(cache[aIndex]) {
-          a = cache[aIndex];
-        }
-        else {
-          a = sortValueForCell(th, self.cellToSort(a), self);
-          cache[aIndex] = a;
-        }
-        // Sort Value B
-        if(cache[bIndex]) {
-          b = cache[bIndex];
-        }
-        else {
-          b = sortValueForCell(th, self.cellToSort(b), self);
-          cache[bIndex]= b;
-        }
-        return (naturalSort(a, b) * direction);
-      });
-
-      rows.each(function(i, tr) {
-        fragment.append(tr);
-      });
-      table.append(fragment.html());
-
-      th.addClass(self.settings[self.direction[index]]);
-
-      self.log('Sort finished in ' + ((new Date()).getTime() - start.getTime()) + 'ms');
-      self.$table.trigger('tablesort:complete', [self]);
-
-    },
-
-    cellToSort: function(row) {
-      return $($(row).find('td').get(this.index));
-    },
-
-
-    log: function(msg) {
-      if(($.tablesort.DEBUG || this.settings.debug) && console && console.log) {
-        console.log('[tablesort] ' + msg);
-      }
-    },
-
-    destroy: function() {
-      this.$table.find('thead th').unbind('click.tablesort');
-      this.$table.data('tablesort', null);
-      return null;
+    if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
+      evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(name, false, false, undefined);
+    } else {
+      evt = new CustomEvent(name);
     }
 
+    return evt;
   };
 
-  $.tablesort.DEBUG = false;
-
-  $.tablesort.defaults = {
-    debug: $.tablesort.DEBUG,
-    asc: 'sorted ascending',
-    desc: 'sorted descending'
+  var getInnerText = function(el) {
+    return el.getAttribute('data-sort') || el.textContent || el.innerText || '';
   };
 
-  $.fn.tablesort = function(settings) {
-    var table, sortable, previous;
-    return this.each(function() {
-      table = $(this);
-      previous = table.data('tablesort');
-      if(previous) {
-        previous.destroy();
+  // Default sort method if no better sort method is found
+  var caseInsensitiveSort = function(a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+
+    if (a === b) return 0;
+    if (a < b) return 1;
+
+    return -1;
+  };
+
+  // Stable sort function
+  // If two elements are equal under the original sort function,
+  // then there relative order is reversed
+  var stabilize = function(sort, antiStabilize) {
+    return function(a, b) {
+      var unstableResult = sort(a.td, b.td);
+
+      if (unstableResult === 0) {
+        if (antiStabilize) return b.index - a.index;
+        return a.index - b.index;
       }
-      table.data('tablesort', new $.tablesort(table, settings));
+
+      return unstableResult;
+    };
+  };
+
+  Tablesort.extend = function(name, pattern, sort) {
+    if (typeof pattern !== 'function' || typeof sort !== 'function') {
+      throw new Error('Pattern and sort must be a function');
+    }
+
+    sortOptions.push({
+      name: name,
+      pattern: pattern,
+      sort: sort
     });
   };
 
-})(jQuery);
+  Tablesort.prototype = {
+
+    init: function(el, options) {
+      var that = this,
+          firstRow,
+          defaultSort,
+          i,
+          cell;
+
+      that.table = el;
+      that.thead = false;
+      that.options = options;
+
+      if (el.rows && el.rows.length > 0) {
+        if (el.tHead && el.tHead.rows.length > 0) {
+          firstRow = el.tHead.rows[el.tHead.rows.length - 1];
+          that.thead = true;
+        } else {
+          firstRow = el.rows[0];
+        }
+      }
+
+      if (!firstRow) return;
+
+      var onClick = function() {
+        if (that.current && that.current !== this) {
+          that.current.classList.remove('sort-up');
+          that.current.classList.remove('sort-down');
+        }
+
+        that.current = this;
+        that.sortTable(this);
+      };
+
+      // Assume first row is the header and attach a click handler to each.
+      for (i = 0; i < firstRow.cells.length; i++) {
+        cell = firstRow.cells[i];
+        if (!cell.classList.contains('no-sort')) {
+          cell.classList.add('sort-header');
+          cell.tabindex = 0;
+          cell.addEventListener('click', onClick, false);
+
+          if (cell.classList.contains('sort-default')) {
+            defaultSort = cell;
+          }
+        }
+      }
+
+      if (defaultSort) {
+        that.current = defaultSort;
+        that.sortTable(defaultSort);
+      }
+    },
+
+    sortTable: function(header, update) {
+      var that = this,
+          column = header.cellIndex,
+          sortFunction = caseInsensitiveSort,
+          item = '',
+          items = [],
+          i = that.thead ? 0 : 1,
+          sortDir,
+          sortMethod = header.getAttribute('data-sort-method'),
+          sortOrder = header.getAttribute('data-sort-order');
+
+      that.table.dispatchEvent(createEvent('beforeSort'));
+
+      // If updating an existing sort `sortDir` should remain unchanged.
+      if (update) {
+        sortDir = header.classList.contains('sort-up') ? 'sort-up' : 'sort-down';
+      } else {
+        if (header.classList.contains('sort-up')) {
+          sortDir = 'sort-down';
+        } else if (header.classList.contains('sort-down')) {
+          sortDir = 'sort-up';
+        } else if (sortOrder === 'asc') {
+          sortDir = 'sort-down';
+        } else if (sortOrder === 'desc') {
+          sortDir = 'sort-up';
+        } else {
+          sortDir = that.options.descending ? 'sort-up' : 'sort-down';
+        }
+
+        header.classList.remove(sortDir === 'sort-down' ? 'sort-up' : 'sort-down');
+        header.classList.add(sortDir);
+      }
+
+      if (that.table.rows.length < 2) return;
+
+      // If we force a sort method, it is not necessary to check rows
+      if (!sortMethod) {
+        while (items.length < 3 && i < that.table.tBodies[0].rows.length) {
+          item = getInnerText(that.table.tBodies[0].rows[i].cells[column]);
+          item = item.trim();
+
+          if (item.length > 0) {
+            items.push(item);
+          }
+
+          i++;
+        }
+
+        if (!items) return;
+      }
+
+      for (i = 0; i < sortOptions.length; i++) {
+        item = sortOptions[i];
+
+        if (sortMethod) {
+          if (item.name === sortMethod) {
+            sortFunction = item.sort;
+            break;
+          }
+        } else if (items.every(item.pattern)) {
+          sortFunction = item.sort;
+          break;
+        }
+      }
+
+      that.col = column;
+      var newRows = [],
+          noSorts = {},
+          j,
+          totalRows = 0,
+          noSortsSoFar = 0;
+
+      for (i = 0; i < that.table.tBodies.length; i++) {
+        for (j = 0; j < that.table.tBodies[i].rows.length; j++) {
+          item = that.table.tBodies[i].rows[j];
+          if (item.classList.contains('no-sort')) {
+            // keep no-sorts in separate list to be able to insert
+            // them back at their original position later
+            noSorts[totalRows] = item;
+          } else {
+            // Save the index for stable sorting
+            newRows.push({
+              tr: item,
+              td: getInnerText(item.cells[that.col]),
+              index: totalRows
+            });
+          }
+          totalRows++;
+        }
+      }
+
+      // Before we append should we reverse the new array or not?
+      // If we reverse, the sort needs to be `anti-stable` so that
+      // the double negatives cancel out
+      if (sortDir === 'sort-down') {
+        newRows.sort(stabilize(sortFunction, true));
+        newRows.reverse();
+      } else {
+        newRows.sort(stabilize(sortFunction, false));
+      }
+
+      // append rows that already exist rather than creating new ones
+      for (i = 0; i < totalRows; i++) {
+        if (noSorts[i]) {
+          // We have a no-sort row for this position, insert it here.
+          item = noSorts[i];
+          noSortsSoFar++;
+        } else {
+          item = newRows[i - noSortsSoFar].tr;
+        }
+
+        // appendChild(x) moves x if already present somewhere else in the DOM
+        that.table.tBodies[0].appendChild(item);
+      }
+
+      that.table.dispatchEvent(createEvent('afterSort'));
+    },
+
+    refresh: function() {
+      if (this.current !== undefined) {
+        this.sortTable(this.current, true);
+      }
+    }
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Tablesort;
+  } else {
+    window.Tablesort = Tablesort;
+  }
+})();
